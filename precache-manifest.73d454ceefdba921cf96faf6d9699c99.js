2a1ab08543b5c177324abbce7a14c2294adb35be
godjinsoo <eiamur01026551290@gmail.com>
2020-09-22 19:11:38 +0900

    Updates

---
+++ precache-manifest.73d454ceefdba921cf96faf6d9699c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5d2651380537f31fd0968b83b3931d",
+    "url": "/mytwitter/index.html"
+  },
+  {
+    "revision": "769f694a584e70e7379c",
+    "url": "/mytwitter/static/css/main.05664297.chunk.css"
+  },
+  {
+    "revision": "ace08bf1d3c3336ea6cd",
+    "url": "/mytwitter/static/js/2.0c70655f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/mytwitter/static/js/2.0c70655f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769f694a584e70e7379c",
+    "url": "/mytwitter/static/js/main.24e3ba8d.chunk.js"
+  },
+  {
+    "revision": "5e1190fba836cb78d9fe",
+    "url": "/mytwitter/static/js/runtime-main.d86328b3.js"
+  }
+]);
